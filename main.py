969800b97193b150c5926fe69d881e7713e76493
W1L74680 <83970923+W1L74680@users.noreply.github.com>
2022-01-03 22:27:27 -0500

    Update main.py

--- main.py
+++ main.py
@@ -2,21 +2,17 @@ import random
 import os
 import time
 
-#create a function that clear the console
 def clear():
     if os.name == "nt":
         os.system("cls")
     else:
         os.system("clear")
 
-#print the random number
 def randomNumber(x, y):
     while True:
-        #the time can be changed
-        time.sleep(1)
         print(random.randint(x, y))
-        time.sleep(1)
+        time.sleep(5)
         clear()
+        time.sleep(5)
 
-#call the function with the numbers
 randomNumber(1, 10)
